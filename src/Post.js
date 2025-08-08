5d7e7a6734026e7381008b082daf0b32644da5e7
Rini K <rinik2014@gmail.com>
2025-08-08 00:59:33 -0700

    added image, resizing formatting

--- src/Post.js
+++ src/Post.js
@@ -1,13 +1,18 @@
 //using the es7 extension
 import React from 'react'
+import './Post.css'
 
 function Post() {
   return (
-    <div>
-        <h3>Username</h3>
+    <div className="post">
         {/* header --> avatar, username */}
+        <h3>Username</h3>
+        
         {/* image */}
+        <img className="post_Image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOxiFZo8hp7oqoftdM8UCkftr2ZDLY0C25LA&s" alt="" />
+        
         {/* username, caption */}
+        <h4>Username: caption</h4>
     </div>
   )
 }
