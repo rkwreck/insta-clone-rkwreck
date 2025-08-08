53c94ba5dbda5c7f9df1fed4c59eeeaa26877323
Rini K <rinik2014@gmail.com>
2025-08-08 00:15:52 -0700

    added Instagram cursive word logo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ function App() {
   return (
     <div className="App">
       <h1>HELLO HELLO THIS IS SO COOL</h1>
+
+
+      <div className="app_header">
+        {/* making the instagram text logo in the header */} 
+        <img
+          className="app_headerImage"
+          src = "https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+  
+
+      {/* Instagram header w/ login buttons and profile picture */}
+
+      {/*we want posts*/}
     </div>
   );
 }
